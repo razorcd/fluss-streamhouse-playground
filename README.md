3dee638d0716a64713f08dee13a77fd955053d83
razor <razorcd@yahoo.com>
2026-02-01 02:45:13 +0100

    adds kafka to fluss flink exec

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
+- Flink: http://localhost:8083
 - `docker exec -ti kafka-broker bash -c "kafka-topics -bootstrap-server localhost:9092 --create --topic rawdatastream"`
 - `docker exec -ti kafka-broker bash -c "kafka-console-producer -bootstrap-server localhost:9092 --topic rawdatastream"`
     - `{"event_id": "event2", "user_id":"1002"}`
-    
--- kafka-flink-fluss-iceberg-demo/src/main/java/com/example/FlinkKafkaSQL.java
+++ kafka-flink-fluss-iceberg-demo/src/main/java/com/example/FlinkKafkaSQL.java
@@ -32,6 +32,35 @@ public class FlinkKafkaSQL {
                         ")";
         tEnv.executeSql(createTableSql).print();
 
+    
+        String fluxxCatalog = 
+                " CREATE CATALOG fluss_catalog \n" +
+                " WITH (\n" +
+                "  'type' = 'fluss',\n" +
+                // "  'default-database' = 'default',\n" +
+                "  'bootstrap.servers' = 'localhost:9123'\n" +
+                ")";
+
+
+        tEnv.executeSql(fluxxCatalog).print();
+        // tEnv.executeSql("USE CATALOG fluss_catalog").print();
+
+        String dropTableIfExistis = "DROP TABLE IF EXISTS fluss_catalog.fluss.fluss_user;";
+        tEnv.executeSql(dropTableIfExistis).print();
+
+        String createFlussTable = "CREATE TABLE fluss_catalog.fluss.fluss_user (\n" + 
+                        "  event_id STRING,\n" +
+                        "  user_id STRING,\n" +
+                        "  event_time TIMESTAMP_LTZ(3)\n" +
+                        ");";
+        tEnv.executeSql(createFlussTable).print(); 
+
+        String insertIntoFluss = 
+                "INSERT INTO fluss_catalog.fluss.fluss_user \n" +
+                "SELECT event_id, user_id, event_time \n" +
+                "FROM default_catalog.default_database.rawdatastreamTable;";
+        tEnv.executeSql(insertIntoFluss);
+
 
 //      {"event_id": "event2", "user_id":"1002"}
 //        String selectSql =
@@ -43,26 +72,38 @@ public class FlinkKafkaSQL {
 ////                "WHERE user_id = '1001'"
 //                ;
 
+        // Print Flink table from Fluss:
+       String selectFluss =
+                "SELECT * \n" +
+                "FROM fluss_catalog.fluss.fluss_user\n" +
+                ";"
+        ;
 
-        // Business logic processing:
+        Table resultFlussTable = tEnv.sqlQuery(selectFluss);
+        DataStream<Row> resultFlussStream = tEnv.toChangelogStream(resultFlussTable);
+        resultFlussStream.print();
 
-        String selectSql2 =
-                "SELECT\n" +
-                        "COUNT(*) AS events_count,\n" +
-                        "user_id\n" +
-                        "FROM rawdatastreamTable\n" +
-                        "WHERE user_id <> '1001'\n" +
-                        "GROUP BY user_id\n" +
-                        ";"
-                ;
 
-        Table resultTable = tEnv.sqlQuery(selectSql2);
+        // Print Flink table from Kafka:
+        // tEnv.executeSql("USE CATALOG default_catalog").print();
 
-        // Convert the unbounded table with aggregation to a changelog DataStream
-        DataStream<Row> resultStream = tEnv.toChangelogStream(resultTable);
+        // String selectSql2 =
+        //         "SELECT\n" +
+        //                 "COUNT(*) AS events_count,\n" +
+        //                 "user_id\n" +
+        //                 "FROM rawdatastreamTable\n" +
+        //                 "WHERE user_id <> '1001'\n" +
+        //                 "GROUP BY user_id\n" +
+        //                 ";"
+        //         ;
 
-        // Process the stream
-        resultStream.print();
+        // Table resultTable = tEnv.sqlQuery(selectSql2);
+
+        // // Convert the unbounded table with aggregation to a changelog DataStream
+        // DataStream<Row> resultStream = tEnv.toChangelogStream(resultTable);
+
+        // // Process the stream
+        // resultStream.print();
 
         // Execute the Flink job
         env.execute("Flink Kafka SQL Job");
